dfda668a1a52eee99b7b3348fdd93fc3df95bbde
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-10-02 20:13:33 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import ReactDOM from "react-dom/client"
+import Header from "./components/Header";
 
 const App = ()=>{
-    return (<>React App Rendering!</>);
+    return (
+        <div className="app-container">
+            <Header />
+        </div>);
 }
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
