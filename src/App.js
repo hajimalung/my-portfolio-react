47bf5356b2b5be998ce26a5517126bd2193ee01b
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-10-02 20:05:20 +0530

    bootstraping react app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-console.log("App loading...")
+import ReactDOM from "react-dom/client"
+
+const App = ()=>{
+    return (<>React App Rendering!</>);
+}
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(<App />);
