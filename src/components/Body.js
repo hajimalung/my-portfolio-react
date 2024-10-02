de2cd28da79a3e48550baa164c3ef30a48e5d5ca
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-10-02 22:39:34 +0530

    adding profile photos

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,23 @@
+// Importing the image
+import profileImage from 'url:../assets/images/bright_profile.jpg';
+
 const Body = ()=>{
+    // Using URL constructor for optimization
+    const imageUrl = new URL(`${profileImage}?width=640&quality=75`, import.meta.url);
+
     return (
-    <div className="app-body">
-        Body works!
-    </div>);
+        (<div className="app-body">
+            <div className="body-left" >
+                <img 
+                className="profile-image"
+                src={`${imageUrl}`} 
+                alt="This is profile image">
+                </img>
+            </div>
+            <div className="body-right">
+                Body Right
+            </div>
+        </div>)
+    );
 }
 export default Body;
