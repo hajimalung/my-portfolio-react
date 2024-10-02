c518d0683fbfa359da8a8a134050e50bdcd39970
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-10-03 00:20:24 +0530

    added routes and pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import ReactDOM from "react-dom/client"
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import AppError from "./components/AppError";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 
 const App = ()=>{
     return (
         <div className="app-container">
             <Header />
-            <Body />
+            <Outlet />
             <Footer />
         </div>);
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element: <App />,
+        children:[
+            {
+            path:"/",
+            element:<Body />,
+            },
+            {
+                path:"/resume",
+                element:<Resume />
+            },
+            {
+                path:"/projects",
+                element:<Projects />
+            },
+            {
+                path:"/contact",
+                element:<Contact />
+            }
+        ],
+        errorElement:<AppError />
+    }
+]);
+
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<App />);
+rootElement.render(<RouterProvider router={appRouter} />);
---
+++ src/components/AppError.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+
+const AppError = ()=>{
+    return (<>
+       Page Under construction mean while get to know about <Link to={"/"}>Hajimalung</Link>
+    </>);
+}
+export default AppError;
